11b063c1488521d4f43486ed2c85e3e7390bccc5
Leonardo Rezende <rezendeoliveira.leonardo@gmail.com>
2021-03-23 21:50:52 -0300

    Projeto teste criado

---
+++ Teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
